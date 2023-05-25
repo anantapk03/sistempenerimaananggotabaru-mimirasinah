3657b694be2b32f46135977537cc64a38110e510
anantapk03 <anantapk03@gmail.com>
2023-05-25 13:45:20 +0700

    Test Error

---
+++ database/migrations/2023_05_25_063041_create_anggotas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnggotasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('anggotas', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('nama');
+            $table->string('alamat');
+            $table->bigInteger('no_wa');
+            $table->string('image');
+            $table->string('password')->default(bcrypt('12345'));
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('anggotas');
+    }
+}
